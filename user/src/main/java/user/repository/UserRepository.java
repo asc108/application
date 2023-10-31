b1025c25add647f3390729fcf8afd848f931d9c8
Drasko Petrovic <asc108108108@gmail.com>
2023-10-31 19:44:53 +0100

    Update UserRepository.java

--- user/src/main/java/user/repository/UserRepository.java
+++ user/src/main/java/user/repository/UserRepository.java
@@ -1,11 +1,16 @@
 package user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import user.model.Users;
 
 public interface UserRepository extends JpaRepository<Users, Integer>{
 	
 	public Users findByEmail(String email);
+	@Query("SELECT u from Users u where u.username = :username")
+	Optional<Users> findUserByUsername(String username);
 
 }
