c030df185cfcb3e3de376eb4627ed0150fd09829
Drasko Petrovic <asc108108108@gmail.com>
2023-11-02 10:08:52 +0100

    Update

--- user/src/main/java/user/controller/UserController.java
+++ user/src/main/java/user/controller/UserController.java
@@ -2,7 +2,11 @@ package user.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,22 +21,32 @@ import user.service.UserService;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
-	
+
 	private final UserService userService;
-	
-	
+
 	@PostMapping("/register")
 	public void registerUser(@Valid @RequestBody UserRegistrationRequest registration) {
 		userService.registerUser(registration);
 	}
+
 	@GetMapping("/allUsers")
 	public List<Users> getAllUsers() {
-		
+
 		return userService.getAllUsers();
 	}
-	@GetMapping("/ok")
-	public String isOK() {
-		return "Ok!";
+	
+	@DeleteMapping("/{username")
+	public ResponseEntity<String> removeUser(@PathVariable String username) {
+		try {
+			userService.removeUser(username);
+			return ResponseEntity.ok("Sucssessfull");
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+		
+		
 	}
+	
+	
 
 }
--- user/src/main/java/user/service/UserService.java
+++ user/src/main/java/user/service/UserService.java
@@ -1,19 +1,20 @@
 package user.service;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.apache.catalina.security.SecurityConfig;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import user.controller.UserRegistrationRequest;
 import user.model.Users;
 import user.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
 	private final UserRepository userRepository;
@@ -22,7 +23,7 @@ public class UserService {
 		Users user = Users.builder().firstName(registration.firstname()).lastName(registration.lastname())
 				.email(registration.email()).username(registration.username()).build();
 		user.setPassword(new BCryptPasswordEncoder().encode(registration.password()));
-		Users userCheck = userRepository.findByEmail(user.getEmail());
+		Optional<Users> userCheck = userRepository.findUserByUsername(user.getUsername());
 		if (userCheck == null) {
 			userRepository.save(user);
 		} else {
@@ -33,6 +34,17 @@ public class UserService {
 	public List<Users> getAllUsers() {
 		List<Users> users = userRepository.findAll();
 		return users;
+	}
+
+	public void removeUser(String username) {
+		Optional<Users> user = userRepository.findUserByUsername(username);
+		if (user.isPresent()) {
+			log.info("found");
+			userRepository.delete(user.get());
+		} else {
+			throw new IllegalArgumentException("Not found user with that username");
+		}
+
 	}
 
 }
