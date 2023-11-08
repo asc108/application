f982541ac95e1047e6b05b4422d42747d82ffcbd
Drasko Petrovic <asc108108108@gmail.com>
2023-11-08 10:54:52 +0100

    User service

--- user/src/main/java/user/UserApplication.java
+++ user/src/main/java/user/UserApplication.java
@@ -2,8 +2,12 @@ package user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients(basePackages = {"feignclients","ampqp"})
+@SpringBootApplication(
+		 scanBasePackages = {"user","ampqp"}
+		)
 public class UserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
--- user/src/main/java/user/service/UserService.java
+++ user/src/main/java/user/service/UserService.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import ampqp.RabbitMQMessageProducer;
+import feignclients.notification.NotificationRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import user.controller.UserRegistrationRequest;
@@ -18,17 +20,22 @@ import user.repository.UserRepository;
 public class UserService {
 
 	private final UserRepository userRepository;
+	private final RabbitMQMessageProducer r;
 
 	public void registerUser(UserRegistrationRequest registration) {
 		Users user = Users.builder().firstName(registration.firstname()).lastName(registration.lastname())
 				.email(registration.email()).username(registration.username()).build();
 		user.setPassword(new BCryptPasswordEncoder().encode(registration.password()));
 		Optional<Users> userCheck = userRepository.findUserByUsername(user.getUsername());
-		if (userCheck == null) {
-			userRepository.save(user);
+		if (userCheck.isEmpty()) {
+			userRepository.saveAndFlush(user);
+			NotificationRequest n = new NotificationRequest(user.getUsername(), String.format("Hi %s,Welcome!",user.getFirstName()));
+			r.publish(n, "internal.exchange", "internal.notification.routing-key");
+
 		} else {
 			throw new IllegalArgumentException("Already registered");
 		}
+
 	}
 
 	public List<Users> getAllUsers() {
