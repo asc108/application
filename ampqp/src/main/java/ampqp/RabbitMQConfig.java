4f015ddff8b8b1882db089aadf493975e4b0cb7b
Drasko Petrovic <asc108108108@gmail.com>
2023-11-08 10:53:15 +0100

    RabbitMQ service

--- ampqp/src/main/java/ampqp/RabbitMQConfig.java
+++ ampqp/src/main/java/ampqp/RabbitMQConfig.java
@@ -1,5 +1,8 @@
 package ampqp;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -9,37 +12,31 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
-
-import lombok.AllArgsConstructor;
-
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class RabbitMQConfig {
-	
+
 	private final ConnectionFactory connectionFactory;
 
-    @Bean
-    public AmqpTemplate amqpTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jacksonConverter());
-        return rabbitTemplate;
-    }
+	@Bean
+	public AmqpTemplate amqpTemplate() {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jacksonConverter());
+		return rabbitTemplate;
+	}
 
-    @Bean
-    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
-        SimpleRabbitListenerContainerFactory factory =
-                new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(jacksonConverter());
-        return factory;
-    }
+	@Bean
+	public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory() {
+		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory);
+		factory.setMessageConverter(jacksonConverter());
+		return factory;
+	}
 
-    @Bean
-    public MessageConverter jacksonConverter() {
-        MessageConverter jackson2JsonMessageConverter =
-                new Jackson2JsonMessageConverter();
-        return jackson2JsonMessageConverter;
-    }
+	@Bean
+	public MessageConverter jacksonConverter() {
+		MessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+		return jackson2JsonMessageConverter;
+	}
 
 }
