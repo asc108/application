e34443889c030a75ff62d412945d83757a8f7c43
Drasko Petrovic <asc108108108@gmail.com>
2023-11-09 08:57:18 +0100

    Feign Client

--- feignclients/src/main/java/feignclients/product/InventoryClient.java
+++ feignclients/src/main/java/feignclients/product/InventoryClient.java
@@ -4,11 +4,23 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @FeignClient("product")
 public interface InventoryClient {
 	@GetMapping("api/product/stock")
 	public List<InventoryResponse> isInStock(@RequestParam("name") List<String> name);
+	
+
+	@PutMapping("api/product/update")
+	public void stockUpdate(@RequestParam String name,@RequestParam Integer quantiy);
+	
+	@GetMapping("/{name}")
+	public ProductResponse productByName(@PathVariable String name);
+		
+	
 
 }
